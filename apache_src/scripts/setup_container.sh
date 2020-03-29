aac845aea21fa942d1eb99f9776844f690471777
cheesecat47 <cheesecat47@gmail.com>
2020-03-29 22:59:08 +0900

    [#5] Install mod_jk.so file
    Change Dockerfile and setup_container.sh to install mod_jk.so file in apache-server
    container.
    
    Change vim_setup.sh to reduce build time.

--- apache_src/scripts/setup_container.sh
+++ apache_src/scripts/setup_container.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-echo "script for apache_server here!!!!"
+echo "Setup apache container"
+printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' '#' # Print a line
+
+# install mod_jk
+wget -c http://apache.tt.co.kr/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.48-src.tar.gz
+tar zxvf tomcat-connector*
+cd tomcat-connectors-1.2.48-src/native/
+chmod +x buildconf.sh
+./configure --with-apxs=/usr/local/apache2/bin/apxs
+make
+make install
+# chcon -u system_u -r object_r -t httpd_modules_t /usr/local/apache2/modules/mod_jk.so
--- apache_src/scripts/vim_setup.sh
+++ apache_src/scripts/vim_setup.sh
@@ -7,7 +7,7 @@ echo "Vim setup\n"
 read -p "Do you want to install Vundle and config with .vimrc file? [y/n] > "
 if [ "${REPLY}" == "y" -o "${REPLY}" == "Y" ]
 then
-    apt install -y git
+    apt install -y git vim
     cd ~
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall
