be6d718346d2ea3ed444e589c3daf4d908468f86
cheesecat47 <cheesecat47@gmail.com>
2020-03-30 09:42:33 +0900

    [#7] Fix details
    Fix details after issue #7.

--- apache_src/scripts/setup_container.sh
+++ apache_src/scripts/setup_container.sh
@@ -4,11 +4,11 @@ echo "Setup apache container"
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' '#' # Print a line
 
 # install mod_jk
+echo "Install mod_jk.so"
 wget -c http://apache.tt.co.kr/tomcat/tomcat-connectors/jk/tomcat-connectors-1.2.48-src.tar.gz
 tar zxvf tomcat-connector*
+rm tomcat-connectors-1.2.48-src.tar.gz
 cd tomcat-connectors-1.2.48-src/native/
-chmod +x buildconf.sh
 ./configure --with-apxs=/usr/local/apache2/bin/apxs
 make
 make install
-# chcon -u system_u -r object_r -t httpd_modules_t /usr/local/apache2/modules/mod_jk.so
